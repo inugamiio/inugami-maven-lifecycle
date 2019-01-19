d24b72f6cf9edd1d34f2e6b34e1babf92e1a9090
Patrick Guillerm <patrickguillerm@gmail.com>
2019-01-19 14:25:54 +0100

    PGU - initial import

---
+++ src/main/java/org/inugami/maven/plugin/lifecycle/LifecycleHandlerDeployPluginMojo.java
@@ -0,0 +1,28 @@
+/* --------------------------------------------------------------------
+ *  						  Inugami  
+ * --------------------------------------------------------------------
+ * 
+ * This program is free software: you can redistribute it and/or modify  
+ * it under the terms of the GNU General Public License as published by  
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.inugami.maven.plugin.lifecycle;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Mojo;
+
+@Mojo(name = "handlerDeployPlugin")
+public class LifecycleHandlerDeployPluginMojo extends AbstractMojo {
+    public void execute() throws MojoExecutionException {
+        new MojoHelper().drawDeco("DEPLOY PLUGIN", ">");
+    }
+}
